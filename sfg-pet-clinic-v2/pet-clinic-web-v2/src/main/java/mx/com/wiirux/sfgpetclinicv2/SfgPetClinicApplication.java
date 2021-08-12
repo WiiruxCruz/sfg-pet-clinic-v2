c4f54035f2fd8b9ec3f506449dda43cf27d77329
WiiruxCruz <hialf.92@gmail.com>
2021-08-12 12:50:38 -0500

    test 4

--- sfg-pet-clinic-v2/pet-clinic-web-v2/src/main/java/mx/com/wiirux/sfgpetclinicv2/SfgPetClinicApplication.java
+++ sfg-pet-clinic-v2/pet-clinic-web-v2/src/main/java/mx/com/wiirux/sfgpetclinicv2/SfgPetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SfgPetClinicV2Application {
+public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgPetClinicV2Application.class, args);
+		SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
 
 }
---
+++ sfg-pet-clinic-v2/pet-clinic-web-v2/src/test/java/mx/com/wiirux/sfgpetclinicv2/SfgPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package mx.com.wiirux.sfgpetclinicv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+	
+	/*
+	@Test
+	void contextLoads() {
+	}
+	*/
+
+}
